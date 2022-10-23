1dc17c6af72720cc8308f6190a263170faab45fc
Saurabh Kumar Deepak <76099401+SKD99@users.noreply.github.com>
2022-10-23 12:07:34 +0530

    Add files via upload
    Program to use Various data types in C++ Programming languages.

---
+++ Datatypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=123;
+cout<< a << endl;
+bool bl = true;
+cout<< bl<<endl;
+float f=1.2;
+cout<< f<<endl;
+double d=1.23;
+cout<<d<<endl;
+int size=sizeof(a);
+cout<<"Size of a is:"<< size<< endl;
+
+
+}
